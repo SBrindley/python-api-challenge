d2280d6c84173318f9d86433860e6eaff35f17f9
SBrindley <113051302+SBrindley@users.noreply.github.com>
2022-12-02 23:26:07 +0000

    final push

--- Starter_Code/starter_code/api_keys.py
+++ Starter_Code/starter_code/api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "YOUR KEY HERE"
+weather_api_key = "14097d77fa741efe6900aaf7793cc5b1"
 
 # Geoapify API Key
-geoapify_key = "YOUR KEY HERE"
+geoapify_key = "60dc8dcfe37f43909d247581aa9396fe"
